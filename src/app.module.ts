d2885556280e6d3d5cb179f2b8c09828643368bc
Dinesh Bisht <dineshbisht90@gmail.com>
2026-04-16 19:02:30 +0530

    user entity for database created
    Signed-off-by: Dinesh Bisht <dineshbisht90@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,29 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { PostsController } from './posts/posts.controller';
 import { HashtagsController } from './hashtags/hashtags.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UsersController, PostsController, HashtagsController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'twitter-clone',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logger: 'advanced-console',
+      logging: 'all',
+    }),
+  ],
+  controllers: [
+    AppController,
+    UsersController,
+    PostsController,
+    HashtagsController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/users/user.entity.ts
@@ -0,0 +1,37 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+@Entity('users')
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({
+    type: 'varchar',
+    name: 'user_name',
+    length: 30,
+    nullable: false,
+    unique: true,
+  })
+  userName!: string;
+  @Column({ type: 'varchar', name: 'name', length: 100 })
+  name!: string;
+  @Column({ type: 'varchar', name: 'avatar', length: 255 })
+  avatar!: string;
+  @Column({ type: 'text', name: 'bio' })
+  bio!: string;
+  @Column({ type: 'int', name: 'follower_count', default: 0 })
+  followerCount!: number;
+  @Column({ type: 'int', name: 'followee_count', default: 0 })
+  followeeCount!: number;
+  @Column({ type: 'boolean', name: 'varified', default: false })
+  varified!: boolean;
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt!: Date;
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt!: Date;
+}

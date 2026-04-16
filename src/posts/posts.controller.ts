cd84e2b5f056b6c14b8d8b520f199614928b9bff
Dinesh Bisht <dineshbisht90@gmail.com>
2026-04-16 15:00:00 +0530

    Add all the related routes for posts controller
    Signed-off-by: Dinesh Bisht <dineshbisht90@gmail.com>

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  @Get('/')
+  getAllPosts(): string {
+    return 'Will share all the posts here';
+  }
+  @Get('/:postId')
+  getPostDetails(@Param('postId') postId: string): string {
+    return `Will share the post with ID: ${postId}`;
+  }
+  @Post('/')
+  createNewPost(): string {
+    return 'Will create a new post';
+  }
+  @Delete('/:postId')
+  deletePost(@Param('postId') postId: string): string {
+    return `Will delete the post with ID: ${postId}`;
+  }
+  @Put('/:postId/like')
+  likePost(@Param('postId') postId: string): string {
+    return `Will like the post with ID: ${postId}`;
+  }
+  @Delete('/:postId/like')
+  unlikePost(@Param('postId') postId: string): string {
+    return `Will unlike the post with ID: ${postId}`;
+  }
+}

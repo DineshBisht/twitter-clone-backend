d3db31a43f223ad7ecbc79c6e1c3fdd038192dbc
Dinesh Bisht <dineshbisht90@gmail.com>
2026-04-16 14:59:28 +0530

    Add all the related routes for users controller
    Signed-off-by: Dinesh Bisht <dineshbisht90@gmail.com>

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('/:userId')
+  getUserByUserId(@Param('userId') userId: string): string {
+    return 'getUserByUserId ' + userId;
+  }
+
+  @Get('/username')
+  getUserByUsername(@Param('username') username: string): string {
+    return 'getUserByUsername ' + username;
+  }
+
+  @Post('/')
+  createUser(): string {
+    return 'createUser';
+  }
+
+  @Patch('/:userId')
+  updateUserDetails(@Param('userId') userId: string): string {
+    return 'updateUserDetails ' + userId;
+  }
+  @Put('/:userId/follow')
+  followUser(@Param('userId') userId: string): string {
+    return 'followUser ' + userId;
+  }
+  @Delete('/:userId/follow')
+  unfollowUser(@Param('userId') userId: string): string {
+    return 'unfollowUser ' + userId;
+  }
+  @Get('/:userId/followers')
+  getFollowersOfUsers(@Param('userId') userId: string): string {
+    return 'getFollowers ' + userId;
+  }
+
+  @Put('/:userId/followees')
+  getFolloweesOfUsers(@Param('userId') userId: string): string {
+    return 'get Followees of given user ' + userId;
+  }
+}

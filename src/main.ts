96071dbdf6233e5776e8faac602294d35c68a3be
Dinesh Bisht <dineshbisht90@gmail.com>
2026-04-16 23:37:16 +0530

    configure swagger in project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+  const config = new DocumentBuilder()
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    .setTitle('Twitter Clone Backend API')
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    .setDescription('The Twitter Clone API description')
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    .setVersion('1.0')
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    .addTag('tweets')
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    .build() as any;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();

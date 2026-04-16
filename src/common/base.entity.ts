714039a14bf8b9c39b9228825328d862aae3618d
Dinesh Bisht <dineshbisht90@gmail.com>
2026-04-16 23:23:05 +0530

    created a abstract class called  base entity

---
+++ src/common/base.entity.ts
@@ -0,0 +1,13 @@
+import {
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+export abstract class BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt!: Date;
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt!: Date;
+}

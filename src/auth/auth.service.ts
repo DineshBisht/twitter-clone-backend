1de53b401bec5795d22f98be372ffcb5adb9421a
Dinesh Bisht <dineshbisht90@gmail.com>
2026-04-18 12:38:11 +0530

    bcrypt included to hash user password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private readonly bcrypt: typeof bcrypt;
+
+  constructor() {
+    this.bcrypt = bcrypt;
+  }
+  async hashPassword(password: string): Promise<string> {
+    const saltRounds = this.bcrypt.genSaltSync();
+    return await this.bcrypt.hash(password, saltRounds);
+  }
+  async comparePassword(password: string, hash: string): Promise<boolean> {
+    return await this.bcrypt.compare(password, hash);
+  }
+}
